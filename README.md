34be9ed0cb4cde892a44c263dfd4c1c92ced72e0
hungnd2905 <hungnd2905@gmail.com>
2022-09-19 18:12:15 +0200

    setup SQL server with Docker, add DB context, Connection string

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
 .Net 6 Minimal API
+  
 
-In comparison to ASP.NET MVC API: 
-- Do not has Controller
-- Do not support model validation
-- Do not support for JSONPatch
-- Do not support filters
-- Do not support custom model binding     
-
-Packages:
-- EntityFrameworkCore
-- EntityFrameworkCore.Design
-- EntityFrameworkCore.SqlServer
-- ...
 Commands:
 - dotnet user-secrets init
+- dotnet user-secrets set "UserId" "sa"
+- dotnet user-secrets set "Password" "pa55w0rd!"
+
+- docker-compose up -d
--- SixMinAPI/Program.cs
+++ SixMinAPI/Program.cs
@@ -1,11 +1,27 @@
+
+using AutoMapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SixMinApi.Data;
+using SixMinApi.Dtos;
+using SixMinApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var sqlConBuilder = new SqlConnectionStringBuilder();
+
+sqlConBuilder.ConnectionString = builder.Configuration.GetConnectionString("SQLDbConnection");
+sqlConBuilder.UserID = builder.Configuration["UserID"];
+sqlConBuilder.Password = builder.Configuration["Password"];
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConBuilder.ConnectionString));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
